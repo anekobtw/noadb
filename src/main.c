f1b986f58230213b4298c72219eb441a3d2742b2
anekobtw <elmars0608@gmail.com>
2026-05-30 08:46:42 +0400

    feat: added tables management and a structure.md file

--- src/main.c
+++ src/main.c
@@ -7,6 +7,29 @@ typedef struct {
   char *filename;
 } Database;
 
+void _manage_table_count(Database *db, int new_tables_count) {
+  FILE *file = fopen(db->filename, "r+b");
+  assert(file);
+
+  fseek(file, 0, SEEK_SET);
+  fwrite(&new_tables_count, sizeof(int), 1, file);
+
+  fclose(file);
+}
+
+int get_table_count(Database *db) {
+  FILE *file = fopen(db->filename, "rb");
+  assert(file);
+
+  int table_count;
+  size_t read = fread(&table_count, sizeof(int), 1, file);
+  assert(read == 1);
+
+  fclose(file);
+
+  return table_count;
+}
+
 Database *init_db(char *filename) {
   // checking if it ends with .noa
   size_t filename_l = strlen(filename);
@@ -20,30 +43,48 @@ Database *init_db(char *filename) {
 
   assert(ends_with_noa);
 
-  // creating a file
-  FILE *file = fopen(filename, "w");
+  // create file if it doesn't exist
+  FILE *file = fopen(filename, "ab+");
+
   assert(file);
+
+  fseek(file, 0, SEEK_END);
+  long size = ftell(file);
+
   fclose(file);
 
-  // creating a database object
+  // return a database object
   Database *db = malloc(sizeof(Database));
   assert(db);
 
   db->filename = strdup(filename);
   assert(db->filename);
 
+  if (size < sizeof(int)) {
+    _manage_table_count(db, 0);
+  }
+
   return db;
 }
 
 int main() {
-  printf("Creating a file test.noa\n");
+  printf("Creating database..\n");
 
   Database *db = init_db("test.noa");
 
   if (db) {
     printf("Database created!\n");
     printf("Filename: %s\n", db->filename);
+  } else {
+    printf("Database couldn't be created :(");
+    return 1;
   }
+
+  printf("%d\n", get_table_count(db));
+
+  _manage_table_count(db, 1);
+
+  printf("%d\n", get_table_count(db));
 
   return 0;
 }
---
+++ src/structure.md
@@ -0,0 +1,12 @@
+first number = tables count
+
+then for loop and for each table:
+first number = columns count
+second number = row count
+
+for each column:
+first number = column type (int, str, etc)
+second number = name len
+then name itself
+then reading lines one by one
+
